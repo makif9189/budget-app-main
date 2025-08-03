fc9077878f7afac0a839b9d2d0ee59891c87043b
makif9189 <mehmet.akif.karali@hotmail.com>
2025-08-03 19:55:13 +0300

    03.08.2025 19:55 Güncelleme

---
+++ BudgetApp.Api/Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+namespace BudgetApp.Api.Core.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+}
